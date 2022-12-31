5e9c142bc5ebe18be50aa69a21267017dcac2d4e
Đức Anh <72397861+BuiDucAnh68@users.noreply.github.com>
2023-01-01 00:53:21 +0700

    Update test.js

--- test.js
+++ test.js
@@ -2,8 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export let options = {
-  VUs: 10,
-  duration: '30s',
+  stages: [
+    {duration: '2m', target:40},
+    {duration: '2m', target:50},
+    {duration: '8m', target:0},
+  ],
 };
 
 export default function() {
