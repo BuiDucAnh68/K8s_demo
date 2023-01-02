53f45bcc12308424f49679e4a4e0fc97251f46cf
Đức Anh <72397861+BuiDucAnh68@users.noreply.github.com>
2023-01-03 01:28:10 +0700

    Update test.js

--- test.js
+++ test.js
@@ -16,6 +16,9 @@ export default function () {
   const res = http.get('https://httpbin.test.k6.io/');
   check(res,{
     'is status 200': (r) => r.status === 200,
-    'body size is 11,105 bytes': (r) => r.body.length == 11105,});
+    'body size is 11,105 bytes': (r) => r.body.length == 11105,
+    'Protocol TCP: ': (r) => r.proto === 'TCP',
+    'Protocol is HTTP/2': (r) => r.proto === 'HTTP/2.0',
+  });
   sleep(1);
 }
