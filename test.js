f8001f6d99203e17ebe59848a6c99353e5318b03
Đức Anh <72397861+BuiDucAnh68@users.noreply.github.com>
2023-01-03 00:36:44 +0700

    Update test.js

--- test.js
+++ test.js
@@ -6,6 +6,10 @@ export const options = {
         {duration: '1m', target:60},
 
     ],
+    thresholds: {
+        http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+        http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+  },
 };
 
 export default function () {
