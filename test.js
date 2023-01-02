16befd954aace1cded7572a4cd541ed8bc86576f
Đức Anh <72397861+BuiDucAnh68@users.noreply.github.com>
2023-01-03 00:32:58 +0700

    Update test.js

--- test.js
+++ test.js
@@ -10,7 +10,8 @@ export const options = {
 
 export default function () {
   const res = http.get('https://httpbin.test.k6.io/');
-  check(res,{'verify homepage text': (r) =>
-      r.body.includes('Collection of simple web-pages suitable for load testing'),});
+  check(res,{
+    'is status 200': (r) => r.status === 200,
+    'body size is 11,105 bytes': (r) => r.body.length == 11105,});
   sleep(1);
 }
