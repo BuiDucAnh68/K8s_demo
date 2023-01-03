2d36d658b4c77f6b10871cd913cbcf080b348927
Đức Anh <72397861+BuiDucAnh68@users.noreply.github.com>
2023-01-03 13:17:03 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,29 +1,25 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
-
+import { check,sleep } from 'k6';
 export const options = {
-  stages: [
-    { duration: '2m', target: 100 }, // below normal load
-    { duration: '5m', target: 100 },
-    { duration: '2m', target: 200 }, // normal load
-    { duration: '5m', target: 200 },
-    { duration: '2m', target: 300 }, // around the breaking point
-    { duration: '5m', target: 300 },
-    { duration: '2m', target: 400 }, // beyond the breaking point
-    { duration: '5m', target: 400 },
-    { duration: '10m', target: 0 }, // scale down. Recovery stage.
-  ],
+    stages: [
+        {duration: '30s', target:30},
+        {duration: '1m', target:60},
+
+    ],
+    thresholds: {
+        http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+//         http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+  },
 };
 
 export default function () {
-  const BASE_URL = 'https://test-api.k6.io'; // make sure this is not production
-  const res = 'https://test-api.k6.io'; 
-  const responses = http.batch([
-    ['GET', `${BASE_URL}/public/crocodiles/1/`, null, { tags: { name: 'PublicCrocs' } }],
-    ['GET', `${BASE_URL}/public/crocodiles/2/`, null, { tags: { name: 'PublicCrocs' } }],
-    ['GET', `${BASE_URL}/public/crocodiles/3/`, null, { tags: { name: 'PublicCrocs' } }],
-    ['GET', `${BASE_URL}/public/crocodiles/4/`, null, { tags: { name: 'PublicCrocs' } }],
-  ]);
-
+  const res = http.get('https://httpbin.test.k6.io/');
+  check(res,{
+    'is status 200': (r) => r.status === 200,
+    'body size is 11,105 bytes': (r) => r.body.length == 11105,
+    'Protocol TCP: ': (r) => r.proto === 'TCP',
+    'Protocol is HTTP/2': (r) => r.proto === 'HTTP/2.0',
+    'is OCSP response good': (r) => r.ocsp.status === http.OCSP_STATUS_GOOD,
+  });
   sleep(1);
 }
