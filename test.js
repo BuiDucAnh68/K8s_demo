40051580c1f89e558614e06874450ff1c7ac09b0
Đức Anh <72397861+BuiDucAnh68@users.noreply.github.com>
2023-01-03 09:12:54 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,25 +1,37 @@
 import http from 'k6/http';
-import { check,sleep } from 'k6';
-export const options = {
-    stages: [
-        {duration: '30s', target:30},
-        {duration: '1m', target:60},
+import { check, group, sleep, fail } from 'k6';
 
-    ],
-    thresholds: {
-        http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-//         http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+export const options = {
+  vus: 1, // 1 user looping for 1 minute
+  duration: '1m',
+
+  thresholds: {
+    http_req_duration: ['p(99)<1500'], // 99% of requests must complete below 1.5s
   },
 };
 
-export default function () {
-  const res = http.get('https://httpbin.test.k6.io/');
-  check(res,{
-    'is status 200': (r) => r.status === 200,
-    'body size is 11,105 bytes': (r) => r.body.length == 11105,
-    'Protocol TCP: ': (r) => r.proto === 'TCP',
-    'Protocol is HTTP/2': (r) => r.proto === 'HTTP/2.0',
-    'is OCSP response good': (r) => r.ocsp.status === http.OCSP_STATUS_UNKNOWN,
+const BASE_URL = 'https://test-api.k6.io';
+const USERNAME = 'TestUser';
+const PASSWORD = 'SuperCroc2020';
+
+export default () => {
+  const loginRes = http.post(`${BASE_URL}/auth/token/login/`, {
+    username: USERNAME,
+    password: PASSWORD,
   });
+
+  check(loginRes, {
+    'logged in successfully': (resp) => resp.json('access') !== '',
+  });
+
+  const authHeaders = {
+    headers: {
+      Authorization: `Bearer ${loginRes.json('access')}`,
+    },
+  };
+
+  const myObjects = http.get(`${BASE_URL}/my/crocodiles/`, authHeaders).json();
+  check(myObjects, { 'retrieved crocodiles': (obj) => obj.length > 0 });
+
   sleep(1);
-}
+};
