c265f18d352c898caef9355bb524593b4dfd7532
Đức Anh <72397861+BuiDucAnh68@users.noreply.github.com>
2023-02-20 13:31:20 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,25 +1,56 @@
-import http from 'k6/http';
-import { check,sleep } from 'k6';
-export const options = {
-    stages: [
-        {duration: '30s', target:30},
-        {duration: '1m', target:60},
+import http from "k6/http";
+import { check } from "k6";
 
-    ],
-    thresholds: {
-        http_req_failed: ['rate<0.01'], // http errors should be less than 1%
-//         http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
-  },
+let url, opts, r;
+
+// Element skipped: jmeterTestPlan.hashTree.TestPlan.hashTree.ResultCollector
+// Element skipped: jmeterTestPlan.hashTree.TestPlan.hashTree.ResultCollector
+
+export let options = {
+  stages: [
+    {
+      target: 50,
+      duration: "30s",
+    },
+  ],
 };
 
-export default function () {
-  const res = http.get('https://httpbin.test.k6.io/');
-  check(res,{
-    'is status 200': (r) => r.status === 200,
-    'body size is 11,105 bytes': (r) => r.body.length == 11105,
-    'Protocol TCP: ': (r) => r.proto === 'TCP',
-    'Protocol is HTTP/2': (r) => r.proto === 'HTTP/2.0',
-    'is OCSP response good': (r) => r.ocsp.status === http.OCSP_STATUS_GOOD,
-  });
-  sleep(1);
+export default function (data) {
+  if (__VU >= 1 && __VU <= 50) {
+    /* Created from cURL on 2022-12-28T11:24:27.817 */
+    url =
+      "https://vlnh.mto.zing.vn/wlxxvng-prod/vng/payment/getServersByUserID?clientIP=125.235.136.88&ts=1671642108&userID=2332100573744275456&sig=092a2d0ca6ef879f7c578a68bfbc553f";
+    opts = {
+      redirects: 999,
+    };
+    r = http.request("GET", url, "", opts);
+
+    url =
+      "https://vlnh.mto.zing.vn/wlxxvng-prod/vng/payment/getProducts?loginChannel=10&roleID=1050676753&serverID=1002&ts=1671642112623&userID=2332100573744275456&sig=465ff9dd0650a1b9e75958f260e80e9a";
+    opts = {
+      redirects: 999,
+    };
+    r = http.request("GET", url, "", opts);
+
+    url =
+      "https://vlnh.mto.zing.vn/wlxxvng-prod/vng/payment/getRoles?clientIP=125.235.136.88&loginChannel=10&serverID=1002&ts=1671642111&userID=2332100573744275456&sig=db2e176d717b05b83faa1ba00fe6e6a8";
+    opts = {
+      redirects: 999,
+    };
+    r = http.request("GET", url, "", opts);
+
+    url =
+      "https://vlnh.mto.zing.vn/wlxxvng-prod/vng/payment/getCCU?clientIP=49.213.81.56&ts=1671642175101&sig=ccbb211c13737b753b476dc9fc6fd52e";
+    opts = {
+      redirects: 999,
+    };
+    r = http.request("GET", url, "", opts);
+
+    url =
+      "https://vlnh.mto.zing.vn/wlxxvng-prod/vng/payment/getTrans?amount=10000000&clientID=&country=VN&currency=VND&loginChannel=10&orderNumber=2332118772703322112&productID=web.vlafk.ns40000&roleID=1050676753&serverID=1002&ts=1671642222035&userID=2332100573744275456&sig=817735f600f5bad4ca6092615cec41ed";
+    opts = {
+      redirects: 999,
+    };
+    r = http.request("GET", url, "", opts);
+  }
 }
