726d79a7b0be64ea09f41ce6f66f5dab22d3bd83
Đức Anh <72397861+BuiDucAnh68@users.noreply.github.com>
2023-03-24 14:16:47 +0700

    Update test.js

--- test.js
+++ test.js
@@ -8,7 +8,7 @@ export const requests = new Counter('http_reqs');
 
 export const options = {
   stages: [
-    { target: 100, duration: '1m' },
+    { target: 1000, duration: '1m' },
 //     { target: 1000, duration: '3m'},
   ],
 //   thresholds: {
