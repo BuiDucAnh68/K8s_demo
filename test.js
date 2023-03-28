71d6a789023bff315716ef1237566e8d47953893
Đức Anh <72397861+BuiDucAnh68@users.noreply.github.com>
2023-03-28 13:46:41 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,28 +1,20 @@
 import http from 'k6/http';
 import { sleep, check } from 'k6';
-import { Counter } from 'k6/metrics';
-
-// A simple counter for http requests
-
-export const requests = new Counter('http_reqs');
 
 export const options = {
   stages: [
-    { target: 200, duration: '1m'},
-    { target: 600, duration: '1m' },
-//     { target: 1000, duration: '3m'},
+    { target: 600, duration: '2m' },
+    { target: 0, duration: '1m' }
   ],
-//   thresholds: {
-//     http_reqs: ['count < 100'],
-//   },
+  gracefulStop: '1m',
+  gracefulRampDown: '1m'
 };
 
 export default function () {
 
-  const res = http.get('http://test.k6.io');
 
-  sleep(1);
-
+  const res = http.get("http://test.k6.io",{timeout: "80s"});
+  
   const checkRes = check(res, {
     'status is 200': (r) => r.status === 200,
   });
